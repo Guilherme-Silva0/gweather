d99f1479c59954d27afb580fc8fd7b363503af0c
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-25 14:34:41 -0300

    importing button to switch theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Main from "./components/Main";
+import ThemeButton from "./components/ThemeButton";
+import useTheme from "./hooks/useTheme";
+
 const App = () => {
-  return <div className="text-gray-200">App</div>;
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <>
+      <ThemeButton theme={theme} onClick={toggleTheme} />
+      <Main>
+        <h1 className="text-gray-200 text-4xl font-bold">Eita</h1>
+      </Main>
+    </>
+  );
 };
 
 export default App;
