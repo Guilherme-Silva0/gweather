f9ea2b425f19481432de25d959d5d1f0d7b46859
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-25 21:31:51 -0300

    adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        fist: "#1f2937",
+        second: "#111827",
+        third: "#0099dd",
+      },
       fontFamily: {
         outfit: "'Outfit', sans-serif",
       },
