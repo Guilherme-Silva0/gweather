f762d9e2845d2a598b106ddd66ff324ad520ae5a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-26 17:46:20 -0300

    creating Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { WeatherContext } from "../context/WeatherContext";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Section = ({ children }: Props) => {
+  const { imgLocate } = useContext(WeatherContext);
+  const imgUrl = imgLocate?.urlImg;
+
+  return (
+    <section
+      className="bg-gray-50 rounded-xl shadow-xl bg-no-repeat bg-cover bg-center transition-all max-w-sm max-sm:w-11/12 dark:bg-fist"
+      style={{ backgroundImage: `url(${imgUrl})` }}
+    >
+      <div
+        className={`w-full h-full flex flex-col p-6 gap-3 rounded-xl ${
+          imgUrl === undefined ? "" : " bg-gray-50/70 dark:bg-fist/70"
+        }`}
+      >
+        {children}
+      </div>
+    </section>
+  );
+};
+
+export default Section;
