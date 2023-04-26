ff764a3e3b7d76f1a63684316b4b9856081b3ec9
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-26 17:45:27 -0300

    creating Load component

---
+++ src/components/Load.tsx
@@ -0,0 +1,7 @@
+const Load = () => {
+  return (
+    <div className="border-4 w-6 h-6 border-fist rounded-full border-t-transparent animate-spin dark:border-gray-200 dark:border-t-transparent"></div>
+  );
+};
+
+export default Load;
