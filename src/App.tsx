7175f6890616110c13387c50c3b772857f91e079
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-26 17:44:27 -0300

    building structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import Main from "./components/Main";
+import Section from "./components/Section";
 import ThemeButton from "./components/ThemeButton";
 import useTheme from "./hooks/useTheme";
+import InputGroup from "./components/InputGroup";
+import WeatherInformation from "./components/WeatherInformation";
+import NotFound from "./components/NotFound";
+import { useContext } from "react";
+import { WeatherContext } from "./context/WeatherContext";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
+  const { erro, weather } = useContext(WeatherContext);
+
   return (
     <>
       <ThemeButton theme={theme} onClick={toggleTheme} />
       <Main>
-        <h1 className="text-gray-200 text-4xl font-bold">Eita</h1>
+        <Section>
+          <InputGroup />
+          {erro && <NotFound />}
+          {Object.keys(weather).length !== 0 && <WeatherInformation />}
+        </Section>
       </Main>
     </>
   );
